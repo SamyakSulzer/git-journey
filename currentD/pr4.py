d3933cf367dbaa5a5407fbda0d163e8eb2c53188
Dahale <samyak.dahale@sulzer.com>
2026-03-26 17:35:28 +0530

    msg of 2 fdiles

---
+++ currentD/pr4.py
@@ -0,0 +1,6 @@
+print("Greetings from currentD/pr4.py")
+print("This file was found in a subdirectory.")
+print("Executing some more print logic.")
+print("Python is a versatile language.")
+print("Wrapping up the print commands.")
+print("Task complete for pr4.py.")
