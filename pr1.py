af68e95c69e3915615fb4abdf28aae1078980046
Dahale <samyak.dahale@sulzer.com>
2026-03-26 17:31:42 +0530

    msg of 2 files

---
+++ pr1.py
@@ -0,0 +1,7 @@
+print("Hello from pr1.py")
+print("Learning git practice is fun.")
+print("This file now has multiple lines.")
+print("Print statements help in debugging.")
+print("Finalizing the content for this file.")
+print("All set with pr1.py.")
+#whats up  qef
---
+++ pr2.py
@@ -0,0 +1,7 @@
+print("Hello from pr2.py")
+print("Learning git practice is fun.")
+print("This file now has multiple lines.")
+print("Print statements help in debugging.")
+print("Finalizing the content for this file.")
+print("All set with pr2.py.")
+#whats up 
