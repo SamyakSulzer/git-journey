47df2955b98334c38c6e20f145a1377189911ec1
Dahale <samyak.dahale@sulzer.com>
2026-03-27 19:05:34 +0530

    msg

--- currentD/pr4.py
+++ currentD/pr4.py
@@ -5,8 +5,5 @@ print("Python is a versatile language.")
 print("Wrapping up the print commands.")
 print("Task complete for pr4.py.")
 print("something")
-<<<<<<< HEAD
-b=20
-=======
 a="samyak"
->>>>>>> dev
+
